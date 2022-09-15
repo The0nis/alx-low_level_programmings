b67fbb72d515160f69ba4b5437bb7a4293b8afe1
TheOnis <onisamuelmichael@gmail.com>
2022-09-15 04:04:15 -0700

    update

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
  *  * main - Entry point
  *   *
@@ -8,13 +7,12 @@
  *     */
 int main(void)
 {
-	int i;
+	int num;
 
-	for (i = 0; i < 10; i++)
-		printf("%d", i);
-		printf("\n");
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
 
 	return (0);
 }
-
-
